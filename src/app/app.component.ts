1cccdab3030ebd496f77996bf814236eb399342d
kaushik anand <anandkaushik1424@gmail.com>
2024-05-15 03:51:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
-
+import { Component } from '@angular/core';
+import { GithubService } from './services/github.service';
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//     // Other plugins as needed
+//   ],
+// };
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
-
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+export class AppComponent {
+title = 'responsive-webpage';
+username: string = '';
+userDetails: any;
+repositories: any[] = [];
+pagedRepositories: any[][] = [];
+currentPage: number = 0;
+itemsPerPage: number = 10;
+itemsPerPageOptions: number[] = [10, 25, 50, 100];
+loading: boolean = false; // Add the loading property
+constructor(private githubService: GithubService) {}
+searchUser() {
+if (this.username) {
+this.loading = true; // Set loading to true when the search starts
+this.githubService.getUserDetails(this.username)
+.subscribe(
+  (data) => {
+  this.userDetails = data;
+  this.searchRepositories();
+  },
+  (error) => {
+  console.error('Error fetching user details:', error);
+  this.loading = false; // Set loading to false in case of an error
   }
-}
+  );
+  }
+  }
+  searchRepositories() {
+  if (this.username) {
+  this.githubService.getRepositories(this.username)
+  .subscribe(
+  (data: any[]) => {
+  this.repositories = data;
+  this.setPagedRepositories();
+  this.loading = false; // Set loading to false when data fetching is complete
+  },
+  (error) => {
+  console.error('Error fetching repositories:', error);
+  this.loading = false; // Set loading to false in case of an error
+  }
+  );
+  }
+  }
+  setPagedRepositories() {
+    this.pagedRepositories = [];
+    for (let i = 0; i < this.repositories.length; i += this.itemsPerPage) {
+    this.pagedRepositories.push(this.repositories.slice(i, i + this.itemsPerPage));
+    }
+    this.currentPage = 0;
+    }
+    onItemsPerPageChange() {
+    this.setPagedRepositories();
+    }
+    prevPage() {
+      if (this.currentPage > 0) {
+      this.currentPage--;
+      }
+      }
+      nextPage() {
+      if (this.currentPage < this.pagedRepositories.length - 1) {
+      this.currentPage++;
+      }
+      }
+
+      }
